e3e2d6b206a8186171ff24efdd097ee251aa4a7c
cbastable <therightstuff17@gmail.com>
2013-10-21 01:17:58 -0400

    Updated db schema after Devise merge, worked locally

--- db/schema.rb
+++ db/schema.rb
@@ -9,70 +9,91 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130628221917) do
+ActiveRecord::Schema.define(version: 20131020234331) do
 
-  create_table "collections", :force => true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "collections", force: true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "collections", ["name"], :name => "index_collections_on_name"
+  add_index "collections", ["name"], name: "index_collections_on_name", using: :btree
 
-  create_table "kanjis", :force => true do |t|
+  create_table "kanjis", force: true do |t|
     t.string   "kanji"
     t.string   "onyomi"
     t.string   "kunyomi"
     t.string   "english"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "lists", :force => true do |t|
+  create_table "lists", force: true do |t|
     t.integer  "kanji_id"
     t.integer  "collection_id"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
-  add_index "lists", ["collection_id"], :name => "index_lists_on_collection_id"
-  add_index "lists", ["kanji_id", "collection_id"], :name => "index_lists_on_kanji_id_and_collection_id", :unique => true
-  add_index "lists", ["kanji_id"], :name => "index_lists_on_kanji_id"
+  add_index "lists", ["collection_id"], name: "index_lists_on_collection_id", using: :btree
+  add_index "lists", ["kanji_id", "collection_id"], name: "index_lists_on_kanji_id_and_collection_id", unique: true, using: :btree
+  add_index "lists", ["kanji_id"], name: "index_lists_on_kanji_id", using: :btree
 
-  create_table "word_collections", :force => true do |t|
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "word_collections", force: true do |t|
     t.integer  "collection_id"
     t.integer  "word_id"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
-  add_index "word_collections", ["collection_id"], :name => "index_word_collections_on_collection_id"
-  add_index "word_collections", ["word_id", "collection_id"], :name => "index_word_collections_on_word_id_and_collection_id", :unique => true
-  add_index "word_collections", ["word_id"], :name => "index_word_collections_on_word_id"
+  add_index "word_collections", ["collection_id"], name: "index_word_collections_on_collection_id", using: :btree
+  add_index "word_collections", ["word_id", "collection_id"], name: "index_word_collections_on_word_id_and_collection_id", unique: true, using: :btree
+  add_index "word_collections", ["word_id"], name: "index_word_collections_on_word_id", using: :btree
 
-  create_table "word_kanjis", :force => true do |t|
+  create_table "word_kanjis", force: true do |t|
     t.integer  "kanji_id"
     t.integer  "word_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "word_kanjis", ["kanji_id", "word_id"], :name => "index_word_kanjis_on_kanji_id_and_word_id", :unique => true
-  add_index "word_kanjis", ["kanji_id"], :name => "index_word_kanjis_on_kanji_id"
-  add_index "word_kanjis", ["word_id"], :name => "index_word_kanjis_on_word_id"
+  add_index "word_kanjis", ["kanji_id", "word_id"], name: "index_word_kanjis_on_kanji_id_and_word_id", unique: true, using: :btree
+  add_index "word_kanjis", ["kanji_id"], name: "index_word_kanjis_on_kanji_id", using: :btree
+  add_index "word_kanjis", ["word_id"], name: "index_word_kanjis_on_word_id", using: :btree
 
-  create_table "words", :force => true do |t|
+  create_table "words", force: true do |t|
     t.text     "word"
     t.text     "reading"
     t.text     "translation"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
-  add_index "words", ["reading"], :name => "index_words_on_reading"
-  add_index "words", ["translation"], :name => "index_words_on_translation"
-  add_index "words", ["word"], :name => "index_words_on_word"
+  add_index "words", ["reading"], name: "index_words_on_reading", using: :btree
+  add_index "words", ["translation"], name: "index_words_on_translation", using: :btree
+  add_index "words", ["word"], name: "index_words_on_word", using: :btree
 
 end
