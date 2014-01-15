773c96b4b9bdf5e51993471fad08cee1ebfcd72a
cbastable <therightstuff17@gmail.com>
2014-01-14 23:32:11 -0500

    Start to build controller & view for custom user collections

---
+++ app/controllers/custom_collections_controller.rb
@@ -0,0 +1,15 @@
+class CustomCollectionsController < ApplicationController
+	def index
+	  	@custom_collections = current_user.custom_collections.all
+
+	def show
+		@user = current_user
+	  	@collection = Collection.find_by_name(params[:id])
+	  	@kanji_list = List.where(collection_id: @collection.id).order("id DESC")
+	    @kanji = @kanji_list.first
+	    @test_kanji = @kanji_list.first
+	    #@kanji = @kanji_list.first
+	end
+
+
+end
---
+++ test/helpers/custom_collections_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class CustomCollectionsHelperTest < ActionView::TestCase
+end
